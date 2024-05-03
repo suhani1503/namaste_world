461d1ceb1b6168f93d8340f487e0edf4edc09617
Suhani Ghosh <110628616+suhani1503@users.noreply.github.com>
2024-05-03 19:02:43 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar -dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Apna Cart
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
